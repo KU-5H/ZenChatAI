fb5ec44476fb2d2ed412f7f4e3ed338594ed94bc
Sanjith <sanjith.krishnamoorthy@ontariotechu.net>
2024-01-27 15:01:33 -0500

    Textbox

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
   return (
     <>
       <div className="text-white font-extrabold text-5xl font-gotham text-center pt-5">ZENCHAT</div>
+
+
+      <label for="myTextBox" className="text-white">Chat with AI: </label>
+      <input className="border rounded-lg border-1 border-white border-solid bg-transparent text-white"  
+      type="text" id="myTextBox" name="myTextBox" size = "100" placeholder="Enter Text Here" readonly></input>
+      
+
     </>
   )
 }
